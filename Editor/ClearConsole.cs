e627353be8b426b0cde81a56d7105f19df7e5cb5
casteillet <richardchristmann@hotmail.fr>
2024-06-12 16:27:17 +0200

    fix: test version incrementation

--- Editor/ClearConsole.cs
+++ Editor/ClearConsole.cs
@@ -31,21 +31,15 @@ public static class ClearConsole
 	static void OnPlayModeChanged(PlayModeStateChange playModeState)
 	{
 		if (playModeState == PlayModeStateChange.EnteredPlayMode)
-		{
 			EditorSceneManager.activeSceneChanged += OnActiveSceneChanged;
-		}
 		else if (playModeState == PlayModeStateChange.ExitingPlayMode)
-		{
 			EditorSceneManager.activeSceneChanged -= OnActiveSceneChanged;
-		}
 	}
 
 	private static void OnActiveSceneChanged(UnityEngine.SceneManagement.Scene arg0, UnityEngine.SceneManagement.Scene arg1)
 	{
 		if (Enabled)
-		{
 			ClearLog();
-		}
 	}
 
 	static void ClearLog()
@@ -62,8 +56,6 @@ public static class ClearConsole
 
 		GUILayout.Toggle(m_enabled, new GUIContent("Clear Console", "Clear Console on scene changed during play mode"), GUI.skin.GetStyle("Button"), GUILayout.Width(150));
 		if (GUI.changed)
-		{
 			Enabled = !Enabled;
-		}
 	}
 }
